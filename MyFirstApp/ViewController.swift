4659f08a2d1393f9e7bd3cdbc607ae18b156919b
visvi04 <kononova_vika2004@mail.ru>
2022-03-11 15:46:39 +0300

    первоклашка

--- MyFirstApp/ViewController.swift
+++ MyFirstApp/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var helloLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func setNameAction(_ sender: Any) {
+        if !nameTextField.text!.isEmpty{
+        helloLabel.text = "Приветствую, " + nameTextField.text! + ". Приятного настроения"
+            showAlert(name: "Успешно", desc: "Данные введены.")
+        }
+        else{
+            showAlert(name: "Ошибка", desc: "Вы ничего не ввели")
+        }
+    }
+    func showAlert(name: String, desc: String){
+        let alert = UIAlertController(title: name, message:desc, preferredStyle: .alert)
+        let cancel = UIAlertAction(title: "Закрыть", style: .cancel, handler: nil)
+        alert.addAction(cancel)
+        present(alert, animated: true, completion: nil)
+        
+        
+    }
 }
 
